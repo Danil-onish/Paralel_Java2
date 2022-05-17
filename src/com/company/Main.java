e579fe7e1d4ce975eb337190e6cb5080299ef3f4
Danil-onish <105715779+Danil-onish@users.noreply.github.com>
2022-05-17 13:48:57 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int threadNum = rand.Next(4, 10);
 
         ArrClass arrClass = new ArrClass(length, threadNum);
-        System.out.println("Length: " + length + "\nArray");
+        System.out.println("ThreadNum: "+threadNum+"\nLength: " + length + "\nArray");
         for (int i = 0; i < length; i++) {
             System.out.print(arrClass.arr[i] + " ");
         }
